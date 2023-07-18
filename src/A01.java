22846352ca70d29ab649d401edb548fc7e7208e3
Salih ONGUL <117202571+salihong@users.noreply.github.com>
2023-07-18 23:29:30 +0300

    Add files via upload

---
+++ src/A01.java
@@ -0,0 +1,8 @@
+public class A01 {
+    public static void main(String[] args) {
+        System.out.println(" ");
+        //git branch -M main
+        //git remote add origin https://github.com/salihong/ongul-105.git
+        //git push -u origin main
+    }
+}
---
+++ src/A03.java
@@ -0,0 +1,5 @@
+public class A03  {
+    public static void main(String[] args) {
+
+    }
+}
